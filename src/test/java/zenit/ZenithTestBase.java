0d3b9001138446e844efb3c01fc9d5bacd1eb223
Vincent <101572886+patriknotvincent@users.noreply.github.com>
2023-02-22 13:27:14 +0100

    Base testing class + increments

---
+++ src/test/java/zenit/ZenithTestBase.java
@@ -0,0 +1,67 @@
+package zenit;
+
+import javafx.scene.Node;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.ApplicationTest;
+import zenit.ui.TestUI;
+
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Abstract base class for all test classes. Contains necessary methods in order to test methods contained in the GUI,
+ * or pure logic classes that are interlinked to GUI-related operations (IE JavaFX)
+ */
+
+public abstract class ZenithTestBase extends ApplicationTest {
+
+    /**
+     * @BeforeAll Annotation causes the method to be run once before all test cases are run.
+     * @throws Exception Exceptions that can be caused when running the application
+     *
+     * Launches the application once
+     *
+     */
+    @BeforeAll
+    public static void setUp() throws Exception {
+        ApplicationTest.launch(TestUI.class);
+    }
+
+    /**
+     *
+     * @param stage Part of the GUI to test and / or display
+     * @throws Exception Exceptions associated with running the application
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+
+    /**
+     * @AfterEach Annotation causes the method to be run after every test-case in one class
+     * @throws TimeoutException See JavaDoc
+     *
+     * Makes sure to clear all key presses and mouse-presses that are run by the TestFXRobot
+     *
+     */
+    @AfterEach
+    public void afterEachTest() throws TimeoutException {
+        FxToolkit.hideStage();
+        release(new KeyCode[]{});
+        release(new MouseButton[]{});
+    }
+
+    /**
+     *
+     * @param query Object to be found in the .fxml files
+     * @param <T> Object to be returned
+     * @return An object (or node) associated with .fxml files found in the project.
+     */
+    public <T extends Node> T find(final String query) {
+        return (T) lookup(query).queryAll().iterator().next();
+    }
+}
--- src/test/java/zenit/searchinfile/SearchTest.java
+++ src/test/java/zenit/searchinfile/SearchTest.java
@@ -1,15 +1,84 @@
 package zenit.searchinfile;
 
+import javafx.scene.Node;
+import javafx.scene.input.KeyCode;
+import junit.framework.Assert;
 import org.junit.jupiter.api.Test;
+import zenit.ZenithTestBase;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
 
-class SearchTest {
+class SearchTest extends ZenithTestBase {
 
+    private final String FIELD_SEARCH_INPUT = "fldInputField";
+    private final String FIELD_REPLACE_INPUT = "fldReplaceWord";
+    private final String BUTTON_SCROLL_UP = "btnUp";
+    private final String BUTTON_SCROLL_DOWN = "btnDown";
+    private final String BUTTON_ESCAPE = "btnEsc";
+    private final String BUTTON_REPLACE_FIRST = "btnReplaceOne";
+    private final String BUTTON_REPLACE_ALL = "btnReplaceAll";
+    private final String LABEL_SEARCH_OCCURRENCES = "lblOccurrences";
+    private final String FIELD_FILE_TREE = null;
 
+    private String testFileTXT = "/";
+    private String testFileJava = "/";
 
     @Test
-    void searchInFile() {
+    void getFileLocations() throws IOException {
+        String os = System.getProperty("osName");
+        Path start = Paths.get("/");
+        String filePatternTXT = "SearchTest.txt";
+        String filePatternJava = "SearchTest.java";
+
+        Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+
+                AclFileAttributeView aclView = Files.getFileAttributeView(dir, AclFileAttributeView.class);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    @Test
+    void searchInTextFile() throws Exception {
+        // Get test file
+        Path foo = Path.of("src/testfiles/SearchTest.java");
+        InputStream inputStream = getClass().getResourceAsStream(foo.toString());
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+
+        File bar = new File("src/testfiles/SearchTest.java");
+
+        // Getting the search bar, entering input and searching
+        String query = "main";
+
+        Node tree = find("treeView");
+        List<Node> = 
+
+        push(KeyCode.CONTROL, KeyCode.F)
+                .clickOn(FIELD_SEARCH_INPUT)
+                .write(query);
+        // Actual test
+        Assert.assertEquals("1/1", find(LABEL_SEARCH_OCCURRENCES).getAccessibleText());
+    }
+
+    @Test
+    void searchInJavaFile() throws Exception {
+        File foo = new File("src/testfiles/SearchTest.java");
+
     }
 
     @Test
@@ -34,5 +103,10 @@ class SearchTest {
 
     @Test
     void jumpUp() {
+    }
+
+    @Test
+    void searchWindowLabelsTest() {
+
     }
 }
