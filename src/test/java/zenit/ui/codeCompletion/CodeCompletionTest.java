16b26f22aec29f87b598c252721e36ad244de65a
danielols532 <danielolsson.danielolsson@gmail.com>
2023-03-07 10:17:19 +0100

    Create CodeCompletionTest.java

---
+++ src/test/java/zenit/ui/codeCompletion/CodeCompletionTest.java
@@ -0,0 +1,30 @@
+package zenit.ui.codeCompletion;
+
+import org.junit.jupiter.api.Test;
+import zenit.ZenithTestBase;
+
+public class CodeCompletionTest extends ZenithTestBase {
+
+    @Test
+    void dropDown(){
+        /**
+         * Open file "TestFile"
+         * Move caret?
+         * start typing variables (intTest, stringTest, doubleTest)
+         * See dropdown, do something with it? Assert?
+         *
+         */
+    }
+
+    @Test
+    void chooseCompletion(){
+        /**
+         * Open file
+         * move caret?
+         * start typing variables
+         * choose completion
+         * check if completed manually or assert?
+         *
+         */
+    }
+}
